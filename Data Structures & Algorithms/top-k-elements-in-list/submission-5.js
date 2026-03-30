60c113c5062ee8476b57e675e55964e86bf84875
soham-s-dev <soham.sagade.dev@gmail.com>
2026-03-30 13:13:34 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let map = {};
+        let arr = Array.from({length: nums.length + 1}, () => []);
+
+        for(let num of nums) {
+            map[num] = (map[num] || 0) + 1;
+        }
+
+        for(let n in map) {
+           arr[map[n]].push(parseInt(n));
+        }
+
+        let res = [];
+        for(let i = arr.length - 1; i >= 0; i--) {
+            for(let num of arr[i]) {
+                res.push(num)
+                if(res.length === k) return res;
+            }
+        }
+    }
+}
