30ce0ead91d3bb7e266d18b8f018c51614f2d7ec
soham-s-dev <soham.sagade.dev@gmail.com>
2026-03-31 21:45:43 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let charMap = new Array(26).fill(0);
+        let longest = 0;
+        let maxFreq = 0;
+        let l = 0;
+
+        for(let r = 0; r < s.length; r++) {
+            let index = s[r].charCodeAt(0) - 'A'.charCodeAt(0);
+            charMap[index]++;
+            maxFreq = Math.max(maxFreq, charMap[index]);
+
+            let windowLen = r - l + 1;
+            if(windowLen - maxFreq > k) {
+                charMap[s[l].charCodeAt(0) - 'A'.charCodeAt(0)]--;
+                l++;
+            }
+
+            longest = Math.max(longest, r - l + 1);
+        }
+
+        return longest;
+    }
+}
