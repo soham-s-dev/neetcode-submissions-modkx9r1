0c112bc562709090dc0c9221f0bf2f20d73be57b
soham-s-dev <soham.sagade.dev@gmail.com>
2026-04-04 09:32:21 +0530

    Add: gas-station - submission-6

---
+++ Data Structures & Algorithms/gas-station/submission-6.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} gas
+     * @param {number[]} cost
+     * @return {number}
+     */
+    canCompleteCircuit(gas, cost) {
+        let n = gas.length;
+        for(let i = 0; i < gas.length; i++) {
+            let tank = gas[i] - cost[i];
+            if(tank < 0) continue;
+            
+            let j = (i + 1) % n;
+
+            while(j !== i) {
+                tank += gas[j] - cost[j];
+                if(tank < 0) break;
+                j = (j + 1) % n;
+            }
+
+            if(i === j) return i;
+        }
+        return -1;
+    }
+}
