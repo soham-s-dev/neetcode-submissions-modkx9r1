a9d30d293cd6fa656b88571c2759df19b4171b2b
soham-s-dev <soham.sagade.dev@gmail.com>
2026-04-15 21:53:48 +0530

    Add: string-matching-in-an-array - submission-2

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {string[]}
+     */
+
+    //: REDO AT EARLIEST
+    stringMatching(words) {
+        words.sort((a, b) => b.length - a.length);
+        let s = "";
+        let ans = [];
+        for(let word of words) {
+            if(s.includes(word)) ans.push(word);
+            else s += word;
+        }
+        return ans;
+    }
+}
