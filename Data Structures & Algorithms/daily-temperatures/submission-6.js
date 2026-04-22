a8b19f3bdce5537628787d73231d67a9a9541914
soham-s-dev <soham.sagade.dev@gmail.com>
2026-04-22 13:54:07 +0530

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(t) {
+        let stack = [];
+        let result = new Array(t.length).fill(0);
+
+        for(let i = 0; i < t.length; i++) {
+           while(stack[stack.length - 1] && stack[stack.length - 1][0] < t[i]) {
+                let [elem, index] = stack.pop();
+                let diff = i - index;
+                result[index] = diff;
+           }
+           stack.push([t[i], i]);
+        }
+        return result;
+    }
+}
