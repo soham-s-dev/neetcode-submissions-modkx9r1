670d6d7d66206f30463370515f9da78068f8441f
soham-s-dev <soham.sagade.dev@gmail.com>
2026-04-23 15:03:27 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n) {
+        let res = [];
+
+        function backTrack(open, close, s) {
+            if(open === n && close === n) {
+                res.push(s);
+                return;
+            }
+
+            if(open < n) {
+                backTrack(open + 1, close, s + '(');
+            }
+
+            if(close < open) 
+                backTrack(open, close + 1, s + ')');
+        }
+
+        backTrack(0, 0, "");
+        return res;
+    }
+}
