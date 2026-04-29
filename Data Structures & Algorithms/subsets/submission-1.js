d74a50cf968bc4ceb15d31515baf2d823a1ad58a
soham-s-dev <soham.sagade.dev@gmail.com>
2026-04-29 19:33:44 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        let res = [];
+
+        function backTrack(i, curr) {
+            
+            res.push([...curr]); 
+
+            for(let j = i; j < nums.length; j++) {
+                curr.push(nums[j]);
+                backTrack(j + 1, curr);
+                curr.pop();
+            }
+        }
+
+        backTrack(0, []);
+        return res;
+    }
+}
