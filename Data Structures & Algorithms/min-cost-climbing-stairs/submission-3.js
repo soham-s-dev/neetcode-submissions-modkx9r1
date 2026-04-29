0970d5e99ad256b5a6449ee512255f12bec76197
soham-s-dev <soham.sagade.dev@gmail.com>
2026-04-29 15:21:09 +0530

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        let dp = new Array(cost.length + 1).fill(0);
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(let i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[cost.length];
+    }
+}
