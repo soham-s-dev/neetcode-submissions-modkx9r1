c9454482a57351d6be31b1ce0e1bc409d2b6c22d
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-01 22:00:06 +0530

    Add: maximum-subarray - submission-12

---
+++ Data Structures & Algorithms/maximum-subarray/submission-12.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let maxSum = nums[0];
+        let sum = 0;
+
+        for(let i = 0; i < nums.length; i++) {
+            if(sum < 0) {
+                sum = 0;
+            }
+            sum += nums[i];
+            maxSum = Math.max(sum, maxSum);
+        }
+
+        return maxSum;
+    }
+}
