48938429417bc3508c23dbee6dea03c08965082d
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-01 13:29:31 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-2

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-2.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        let max = -1;
+
+        for(let i = arr.length - 1; i >= 0; i--) {
+            let temp = arr[i];
+            arr[i] = max;
+            max = Math.max(temp, max);
+
+        }
+        return arr;
+    }
+}
