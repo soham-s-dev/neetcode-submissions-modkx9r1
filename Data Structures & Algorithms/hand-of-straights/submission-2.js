b8d8c025b9c23163d63c32ab289eca1c585b05d0
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-04 23:02:32 +0530

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} hand
+     * @param {number} groupSize
+     * @return {boolean}
+     */
+    isNStraightHand(hand, groupSize) {
+        let map = hand.reduce((prev, curr) => {
+            prev[curr] = (prev[curr] || 0) + 1;
+            return prev;
+        }, {});
+
+        hand.sort((a, b) => a - b);
+
+        for(let h of hand) {
+            if(map[h] > 0) {
+                for(let i = h; i < h + groupSize; i++) {
+                    if(!map[i]) return false;
+                    map[i]--;
+                }
+            }
+        }
+        return true;
+    }
+}
