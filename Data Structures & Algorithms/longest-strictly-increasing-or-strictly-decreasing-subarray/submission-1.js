7a558657a70d70e046a77d00c4ab803a9a1b5270
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-05 23:16:29 +0530

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-1

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestMonotonicSubarray(nums) {
+        let incr = 1, decr = 1, res = 1;
+
+        for(let i = 0; i < nums.length; i++) {
+            if(nums[i] > nums[i + 1]) {
+                decr += 1;
+                incr = 1;
+            } else if(nums[i] < nums[i + 1]) {
+                incr += 1;
+                decr = 1;
+            } else {
+                incr = 1;
+                decr = 1;
+            }
+            res = Math.max(incr, decr, res);
+        }
+        return res;
+    }
+}
