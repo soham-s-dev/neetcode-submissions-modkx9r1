7f02ea1c787cdd5c5d68074deaadf09b8ad37b2b
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-06 15:02:43 +0530

    Add: buy-two-chocolates - submission-5

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-5.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @param {number} money
+     * @return {number}
+     */
+    buyChoco(prices, money) {
+       let price1 = Infinity, price2 = Infinity;
+
+        for(let price of prices) {
+            if(price < price1) {
+                price2 = price1;
+                price1 = price;
+            } else if(price >= price1 && price < price2) {
+                price2 = price;
+            }
+            console.log(price1, price2);
+        }
+
+        return money >= price1 + price2 ? money - (price1 + price2) : money; 
+    }
+}
