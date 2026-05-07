2f7e6c949c61edcfff97e07ef2b1efd87cbe4d78
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-07 08:25:05 +0530

    Add: search-for-word - submission-5

---
+++ Data Structures & Algorithms/search-for-word/submission-5.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string} word
+     * @return {boolean}
+     */
+    exist(board, word) {
+        let m = board.length, n = board[0].length;
+        let k = 0;
+        let dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+        for(let i = 0; i < m; i++) {
+            for(let j = 0; j < n; j++) {
+                if(backTrack(i, j, k)) return true;
+            }
+        }
+
+        return false;
+
+
+        function backTrack(i, j, k) {
+            if(k === word.length) return true;
+
+            if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] !== word[k])
+                return false;
+
+            let temp = board[i][j];
+            board[i][j] = '#';
+
+            for(let [x, y] of dirs) {
+                if(backTrack(i + x, j + y, k + 1)) return true;
+            }
+
+            board[i][j] = temp;
+            return false;
+        }
+    }
+}
