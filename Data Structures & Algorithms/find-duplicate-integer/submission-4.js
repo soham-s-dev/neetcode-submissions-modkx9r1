db05a685de8e79169065badf2b6d03745441eaaa
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-07 15:02:29 +0530

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        let l = 0, r = nums.length - 1;
+     
+
+        while(l < r) {
+            let mid = Math.floor((l + r) / 2);
+
+            let count = 0;
+            for(let i = 0; i < nums.length; i++) {
+                if(nums[i] <= mid) count++;
+            }
+
+            if(count > mid) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+}
