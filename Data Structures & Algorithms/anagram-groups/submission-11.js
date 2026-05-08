6f4aa8fe12f59699170359502988bc6817c4c60a
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-08 13:44:22 +0530

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = {};
+
+        for(let str of strs) {
+            let temp = str.split('').sort().join();
+
+            if(map[temp])
+                map[temp].push(str);
+            else 
+                map[temp] = [str];
+        }
+
+        return Object.values(map);
+    }
+}
