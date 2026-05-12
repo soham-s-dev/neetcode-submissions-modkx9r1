5f283db5b844681244792cad43aec010dc1cff64
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-12 12:07:11 +0530

    Add: maximum-number-of-points-with-cost - submission-1

---
+++ Data Structures & Algorithms/maximum-number-of-points-with-cost/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @return {number}
+     */
+    maxPoints(points) {
+        let m = points.length, n = points[0].length;
+        let dp = Array.from({length: m}, () => new Array(n).fill(0));
+
+        for(let c = 0; c < n; c++) {
+            dp[0][c] = points[0][c];
+        }
+
+        for(let r = 1; r < m; r++) {
+            for(let c = 0; c < n; c++) {
+                let max = 0;
+                for(let i = 0; i < n; i++) {
+                    max = Math.max(dp[r - 1][i] - Math.abs(c - i), max);
+                }
+                dp[r][c] = points[r][c] + max;
+            }
+        }
+        console.log(dp);
+        return Math.max(...dp[m - 1]);
+    }
+}
