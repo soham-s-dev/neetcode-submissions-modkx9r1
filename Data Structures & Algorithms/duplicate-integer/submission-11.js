53e2c89cd6b2b2aa631581cf25ffa1ad997feb41
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-13 20:16:40 +0530

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let map = {};
+
+        for(let num of nums) {
+            if(map[num]) return true;
+            map[num] = (map[num] || 0) + 1;
+        }
+        return false;
+    }
+}
