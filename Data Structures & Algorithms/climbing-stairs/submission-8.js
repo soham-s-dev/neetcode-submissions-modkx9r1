c0a02b63cf661350d8603a8409ba1937aa50101a
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-14 11:23:37 +0530

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        let dp = new Array(n + 1).fill(0);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(let i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
