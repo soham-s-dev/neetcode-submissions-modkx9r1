25204c276ddbed80a4f3358300ec92df6b325adf
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-15 10:23:23 +0530

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    lengthOfLIS(nums) {
+        let map = {};
+
+        function dfs(i, j = -1) {
+            if(i >= nums.length) {
+                return 0;
+            }
+
+            if(map[`${i},${j}`]) return map[`${i},${j}`];
+
+            let max1 = 0, max2 = 0;
+            if(j === -1 || nums[j] < nums[i]) {
+                max1 = 1 + dfs(i + 1, i);
+            }
+
+            max2 = dfs(i + 1, j);
+
+            map[`${i},${j}`] = Math.max(max1, max2);
+            return map[`${i},${j}`];
+        }
+
+        return dfs(0);
+    }
+}
