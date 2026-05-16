f713a3ba217f0d372cda7a7e5aba13ef81bcac36
soham-s-dev <soham.sagade.dev@gmail.com>
2026-05-16 14:01:04 +0530

    Add: coin-change - submission-7

---
+++ Data Structures & Algorithms/coin-change/submission-7.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins: number[], amount: number): number {
+        let dp = new Array(amount + 1).fill(Infinity);
+
+        dp[0] = 0;
+
+        for(let i = 1; i <= amount; i++) {
+            for(let coin of coins) {
+                if(i - coin >= 0)
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+        return dp[amount] === Infinity ? -1 : dp[amount];
+    }
+}
